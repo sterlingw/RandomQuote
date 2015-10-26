56f5543e78859bd1c3bc1d5bb391b0703a4fd4dc
sterling <sterlingsrq@gmail.com>
2015-10-26 18:32:54 -0400

    Update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -11,6 +11,7 @@ module.exports = function(config) {
             'extension/js/BackgroundColor.js',
             'extension/js/RandomQuote.js',
             'test/*.spec.js'
-        ]
+        ],
+        singleRun: true
     });
 };
