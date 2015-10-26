8c719aa6c092b099df2c8fa5f82bac68e8cadcee
sterling <sterlingsrq@gmail.com>
2015-10-26 18:29:27 -0400

    Add travis

--- extension/js/app.js
+++ extension/js/app.js
@@ -6,7 +6,5 @@ randomQuote.init(document.getElementById('quote'));
 var backgroundColor = Object.create(BackgroundColor);
 backgroundColor.init(document.body);
 
-
-
 randomQuote.setQuote();
 backgroundColor.setBackgroundColor();
